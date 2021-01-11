d3d0608be0d16c1aef4715334c75f47dc2ab8731
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-01-10 21:41:20 -0800

    added 5 entries into burgers table

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Messy Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mystery Meat Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pickled Burger', false);
