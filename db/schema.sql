277886f170621a3d1fda3eb94e3949d6baaa1cf9
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-01-10 21:40:15 -0800

    creating burgers database and table

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    burger_name VARCHAR(100) NOT NULL,
+    devoured BOOLEAN default false
+);
