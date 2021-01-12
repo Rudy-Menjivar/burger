d673f04becb67726f0747af4cf1d12ad5282d69d
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-01-12 07:20:56 -0800

    Turn an array of question marks into a string

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,11 @@
 // Import MySQL connection
 var connection = require("../config/connection.js");
+
+// Turn an array of question marks into a string
+function printQuestionMarks(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+    return arr.toString();
+};
