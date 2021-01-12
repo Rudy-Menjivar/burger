36e9f674d864458ccccae0ccc910446fce436be1
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-01-12 09:51:40 -0800

    capitalized false

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
 USE burgers_db;
 
-INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Messy Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Mystery Meat Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Pickled Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Messy Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mystery Meat Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pickled Burger', FALSE);
