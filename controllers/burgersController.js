d0000c90dc8fdbd8806693dfbd48c30543541754
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-01-12 16:51:39 -0800

    Added primary/home get route

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -5,4 +5,15 @@ var router = express.Router();
 // Import the model to use its db functions
 var burger = require("../models/burger.js");
 
+// Get route => index
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var burgerFlavors = {
+            burger_flavor: data
+        };
+        console.log(burgerFlavors);
+        res.render("index", burgerFlavors);
+    });
+});
+
 module.exports = router;
