263b5f445057337e51115d9b35cf4995543e78b0
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-01-13 15:03:09 -0800

    Create Post function added for adding new burger flavors

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -16,4 +16,12 @@ router.get("/", function(req, res) {
     });
 });
 
+router.post("/burgers/create", function (req, res) {
+    burger.create(req.body.burger_name, function (newBurger) {
+        console.log("New Burger: " + req.body.burger_name);
+        console.log(newBurger)
+        res.redirect("/");
+    });
+});
+
 module.exports = router;
