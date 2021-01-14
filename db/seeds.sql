eae6d6f200733dd7418f2fcc7f150851614898ac
Rudy Menjivar <rudy.menjivar@gmail.com>
2021-01-14 15:30:55 -0800

    updated last burger name

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,4 +4,4 @@ INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Burger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Messy Burger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible Burger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Mystery Meat Burger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Pickled Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bunless Burger', FALSE);
